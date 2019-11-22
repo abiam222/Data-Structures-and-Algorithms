5ac2b92157b0761a402123df1162aca924b83e6f
Abiam Velazquez <abiam222@gmail.com>
2019-11-22 17:16:20 -0500

    restructure directory

---
+++ Javascript/OOP/p1.js
@@ -0,0 +1,14 @@
+/*
+
+netflix and amazon
+
+{
+    {abiam, flowers, 24344, student}
+    {nick, pieces, 23555, teacher}
+    {nick, pieces, 12312, student}
+    {abiam, flowers, 24344, student}
+}
+
+
+
+*/
